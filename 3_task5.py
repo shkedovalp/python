345157e0e3e9d29628637e577a18ccc23329b3c5
Liubov <shkedovalp@gmail.com>
2021-04-09 23:35:54 +0700

    Практическая работа №3.

--- 3_task5.py
+++ 3_task5.py
@@ -6,16 +6,16 @@
 # Если специальный символ введен после нескольких чисел, то вначале нужно добавить сумму
 # этих чисел к полученной ранее сумме и после этого завершить программу.
 
-# def exe():
-#     result = 0
-#     try:
-#         while True:
-#             for i in map(int, input('Введите строку чисел или введите N для выхода: ').split()):
-#                 result += i
-#             print(result)
-#     except ValueError:
-#         print(result)
-#
-# print(exe())
+def exe():
+    result = 0
+    try:
+        while True:
+            for i in map(int, input('Введите строку чисел или введите N для выхода: ').split()):
+                result += i
+            print(result)
+    except ValueError:
+        print(result)
+
+print(exe())
 
 
